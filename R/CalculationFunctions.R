0185c62a412634c644f04802eebd580f525370bd
Mo Li <mo.vico.li@gmail.com>
2019-12-03 11:05:22 -0600

    Make model calculation flexible for different demands and domestic requirements, fixes https://github.com/USEPA/useeior/issues/7

--- R/CalculationFunctions.R
+++ R/CalculationFunctions.R
@@ -3,45 +3,37 @@
 #' Calculate total emissions/resources (LCI) or total impact for USEEIO model for a given demand vector and perspective.
 #' @param model A complete EEIO model: a list with USEEIO model components and attributes.
 #' @param perspective Perspective of the model, can be "DIRECT", "INTERMEDIATE", or "FINAL".
+#' @param demand A character value or a matrix: if the former, can be "production" or "consumption"; if the latter, can be demand matrix for one or more sectors.
+#' @param use_domestic A boolean value: if TRUE, use domestic A and FinalDemand matrices; if FALSE, use original A and FinalDemand matrices.
 #' @export
 #' @return A list with LCI and LCIA results of the EEIO model.
-calculateEEIOModel <- function(model, perspective, demand="production", use_domestic=FALSE) {
+calculateEEIOModel <- function(model, perspective, demand = "production", use_domestic = FALSE) {
   result <- list()
-
-  ##NOT WORKING - Just outlined
+  # Generate Direct Requirements (A) matrix and Demand dataframe flexibly based on "use_domestic"
   if (use_domestic) {
-    A <- A_d
-    Demand <- DomesticDemand
-  } 
-  
-  if (demand=="production") {
-    f=as.matrix(model$USTotalProduction)
-  } else if (demand="consumption") {
-    f=as.matrix(model$USTotalConsumption)
-
-  } else if (class(demand)=="matrix") {
-    
-  } 
-  
-  # Generate Demand and DomesticDemand vector
-  model$f_d <- as.matrix(rowSums(model$DomesticFinalDemand))
-  # Translates M from producer to purchaser price (M_bar)
-  logging::loginfo("Adjusting total emissions per dollar from producer to purchaser prices...")
-  PHI_C <- as.vector(model$IndustryMargins$PRObyPURRatios)
-  #! PHI_C <- as.vector(model$FinalConsumerMargins$PRObyPURRatios)
-  if (model$specs$CommoditybyIndustryType=="Commodity") {
-    result$M_bar <- model$M %*% diag(PHI_C)
-    colnames(result$M_bar) <- model$Commodities
+    A <- model$A_d
+    Demand <- model$DomesticFinalDemand
   } else {
-    CM <- generateCommodityMixMatrix(model)
-    PHI_I <- as.vector(PHI_C %*% CM)
-    result$M_bar <- model$M %*% diag(PHI_I)
-    colnames(result$M_bar) <- model$Industries
+    A <- model$A
+    Demand <- model$FinalDemand
   }
+  # Generate f (vector) matrix based on "demand"
+  if (demand=="production") {
+    f <- as.matrix(rowSums(Demand))
+  } else if (demand=="consumption") {
+    f <- as.matrix(rowSums(Demand[, model$BEA$TotalConsumptionCodes]))
+  } else if (class(demand)=="matrix") {
+    # Replace sectors in Demand with the sectors from demand
+    Demand_adj <- rbind(Demand[!rownames(Demand)%in%rownames(demand), ], demand)
+    # Order row sequence of Demand_adj to be the same with that of Demand
+    Demand_adj <- Demand_adj[match(rownames(Demand), rownames(Demand_adj)),]
+    f <- as.matrix(rowSums(Demand_adj))
+  }
+  # DirectPerspectiveLCI and DirectPerspectiveLCIA
   if (perspective=="DIRECT") {
     # Calculate DirectPerspectiveLCI (transposed m_d with total impacts in form of sectorxflows)
     logging::loginfo("Calculating Direct Perspective LCI...")
-    c <- getScalingVector(model$L, model$f)
+    c <- getScalingVector(model$L, f)
     result$m_d <- calculateDirectPerspectiveLCI(model$B, c)
     # Normalize DirectPerspectiveLCI
     logging::loginfo("Normalize Direct Perspective LCI...")
