5bfe8fcc391f1501519b6007ac5aa92eee5bacd8
Mo Li <mo.vico.li@gmail.com>
2019-12-06 15:50:51 -0800

    Add components in test.R

--- build-scripts/test.R
+++ build-scripts/test.R
@@ -6,4 +6,8 @@ result <- calculateEEIOModel(model1, "DIRECT")
 result_domestic <- calculateEEIOModel(model1, "DIRECT", use_domestic = TRUE)
 result_external_using_domestic <- calculateEEIOModel(model1, "DIRECT", use_domestic = TRUE, 
                                                      for_imports_using_domestic=TRUE)
+price_adjusted_result <- adjustMultiplierPrice(2017, purchaser_price=TRUE, margin_type="final consumer", model1)
 writeModelComponents(model1)
+
+# Normalize the M result
+m_d_n <- useeior::normalizeResultMatrixByTotalImpacts(result$m_d)
