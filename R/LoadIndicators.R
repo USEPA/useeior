9c43a21a7e885de1aeb7e3a39547cbf2838cca68
Mo Li <mo.vico.li@gmail.com>
2020-01-22 12:15:07 -0800

    Move LCIA functions to LoadIndicators.R

--- R/LoadIndicators.R
+++ R/LoadIndicators.R
@@ -1,13 +1,13 @@
 #' Load indicator factors in a list based on model config.
-#' @param specs specifications of the model.
-#' @return a list of indicator factors not yet formatted for IOMB.
+#' @param specs Specifications of the model.
+#' @return A list of indicator factors not yet formatted for IOMB.
 loadindicators <- function(specs) {
    logging::loginfo('Getting model indicators...')
 
    StaticIndicatorFactors <- loadLCIAfactors()
 
-   #Static LCIA handling - getting factors from LCIA_factors for those indicators used in this model
-   #create a list for appending abbreviations of those staticsource
+   # Static LCIA handling - getting factors from LCIA_factors for those indicators used in this model
+   # Create a list for appending abbreviations of those staticsource
    abbr_from_static <- c()
    for (ind in specs$Indicators) {
       if(ind$StaticSource) {
@@ -16,15 +16,50 @@ loadindicators <- function(specs) {
          abbr_from_static <- append(abbr_from_static,abbr)
       }
    }
-   #Subset LCIA factors list for the list of abbbreviations
+   # Subset LCIA factors list for the list of abbbreviations
    factors_from_static <- subset(StaticIndicatorFactors, Abbreviation %in% abbr_from_static)
-   #Place holder to later rbind in factors_from_dynamic
+   # Place holder to later rbind in factors_from_dynamic
    indicators <- factors_from_static
 
    return(indicators)
 }
 
+#' Deprecated function for generating LCIA output using LCIA_indicators static file
+#' @param model A complete EEIO model: a list with USEEIO model components and attributes.
+#' @return A LCIA dataframe not yet formatted for IOMB.
+generateLCIA <- function (model) { # version is a string value, e.g. 'v1.2'
+   # Load LCIA factors
+   lciafactors <- loadLCIAfactors()
+   # Import LCIA indicators
+   lciaindicators <- utils::read.table(system.file("extdata", "USEEIO_LCIA_Indicators.csv", package = "useeior"),
+                                 sep = ",", header = TRUE, stringsAsFactors = FALSE, check.names = FALSE)
+   indicators <- as.vector(unlist(lapply(model$specs$Indicators, FUN = `[[`, "Abbreviation")))
+   if ("GHG"%in%indicators==TRUE) indicators <- c(indicators, "MGHG", "OGHG")
+   lciaindicators <- lciaindicators[lciaindicators$Abbreviation%in%indicators, ]
+   # Merge LCIA factors and indicators to get meta data
+   lcia <- merge(lciafactors, lciaindicators, by = "Abbreviation")
+   # Drop unit of kg
+   lcia$Unit <- lcia$Units
+   lcia$Units <- NULL
+   return(lciafactorformatted)
+}
 
-
-
+#' Loads all LCIA factors from static source file after melting it to long file
+#' Returns df with "Name""Category""Subcategory""Unit""UUID""Abbreviation""Amount"
+loadLCIAfactors <- function() {
+   lciafact <- utils::read.table(system.file("extdata", "USEEIO_LCIA_Factors.csv", package = "useeior"),
+                                 sep = ",", header = TRUE, stringsAsFactors = FALSE, check.names = FALSE)
+   #Melt these so there is one indicator score per line
+   lciafactlong <- reshape2::melt(lciafact, id.vars = c(1:5))
+   #Convert variable to character
+   lciafactlong$variable <- as.character(lciafactlong$variable)
+   #Convert values to numeric
+   lciafactlong$value <- as.numeric(lciafactlong$value)
+   #drop zeroes
+   lciafactlong <- subset(lciafactlong, value>0)
+   #Change colname for merging later
+   names(lciafactlong)[names(lciafactlong) == "variable"] <- "Abbreviation"
+   names(lciafactlong)[names(lciafactlong) == "value"] <- "Amount"
+   return(lciafactlong)
+}
 
