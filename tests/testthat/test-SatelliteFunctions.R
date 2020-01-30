8b3e50c7c9ece7d39957ef522f93eb98350c1249
Mo Li <mo.vico.li@gmail.com>
2020-01-30 13:23:21 -0800

    Add tests to compare gross output from GDP and BEAIO

--- tests/testthat/test-SatelliteFunctions.R
+++ tests/testthat/test-SatelliteFunctions.R
@@ -34,4 +34,38 @@ test_that("Aggregation of satellite table is working", {
   show_failure(expect_equal(sum(totals_by_sector[totals_by_sector$FlowName==GHG, "FlowAmount"]),
                             sum(totals_by_sector_agg[totals_by_sector_agg$FlowName==GHG, "FlowAmount"])))
   
+  model <- buildEEIOModel("USEEIOv2.0.12s-GHG")
+  totals_by_sector <- utils::read.table(system.file("extdata", model$specs$SatelliteTable$GHG$StaticFile, package = "useeior"),
+                                        sep = ",", header = TRUE, stringsAsFactors = FALSE)
+  totals_by_sector_agg <- aggregateSatelliteTable(totals_by_sector, model$specs$SatelliteTable$GHG$SectorListLevel, model$specs$BaseIOLevel, model)
+  
+  GHG <- "CO2"
+  show_failure(expect_equal(sum(totals_by_sector[totals_by_sector$FlowName==GHG, "FlowAmount"]),
+                            sum(totals_by_sector_agg[totals_by_sector_agg$FlowName==GHG, "FlowAmount"])))
+  
+  GHG <- "CH4"
+  show_failure(expect_equal(sum(totals_by_sector[totals_by_sector$FlowName==GHG, "FlowAmount"]),
+                            sum(totals_by_sector_agg[totals_by_sector_agg$FlowName==GHG, "FlowAmount"])))
+  
+  GHG <- "N2O"
+  show_failure(expect_equal(sum(totals_by_sector[totals_by_sector$FlowName==GHG, "FlowAmount"]),
+                            sum(totals_by_sector_agg[totals_by_sector_agg$FlowName==GHG, "FlowAmount"])))
+  
+  model <- buildEEIOModel("USEEIOv2.0.12is-GHG")
+  totals_by_sector <- utils::read.table(system.file("extdata", model$specs$SatelliteTable$GHG$StaticFile, package = "useeior"),
+                                        sep = ",", header = TRUE, stringsAsFactors = FALSE)
+  totals_by_sector_agg <- aggregateSatelliteTable(totals_by_sector, model$specs$SatelliteTable$GHG$SectorListLevel, model$specs$BaseIOLevel, model)
+  
+  GHG <- "CO2"
+  show_failure(expect_equal(sum(totals_by_sector[totals_by_sector$FlowName==GHG, "FlowAmount"]),
+                            sum(totals_by_sector_agg[totals_by_sector_agg$FlowName==GHG, "FlowAmount"])))
+  
+  GHG <- "CH4"
+  show_failure(expect_equal(sum(totals_by_sector[totals_by_sector$FlowName==GHG, "FlowAmount"]),
+                            sum(totals_by_sector_agg[totals_by_sector_agg$FlowName==GHG, "FlowAmount"])))
+  
+  GHG <- "N2O"
+  show_failure(expect_equal(sum(totals_by_sector[totals_by_sector$FlowName==GHG, "FlowAmount"]),
+                            sum(totals_by_sector_agg[totals_by_sector_agg$FlowName==GHG, "FlowAmount"])))
+  
 })
