2313c3a40c750bc8943d66fe65c2bc2fbe425bc8
Mo Li <mo.vico.li@gmail.com>
2020-02-26 13:52:47 -0800

    Modify formatIOTableforIOMB function to accomodate CxC and IxI tables

--- R/FormattingFunctions.R
+++ R/FormattingFunctions.R
@@ -4,32 +4,35 @@
 #' @param model A complete EEIO model: a list with USEEIO model components and attributes.
 #' @return A dataframe, having IO table structure, with row and column names from "code" to "code/names/locationcode".
 formatIOTableforIOMB <- function (IOtable, model) {
-  # Load pre-saved IndustryCodeName and CommodityCodeName tables
+  # Load model-specific IndustryCodeName and CommodityCodeName tables
   IndustryCodeName <- get(paste(model$specs$BaseIOLevel, "IndustryCodeName", model$specs$BaseIOSchema, sep = "_"))
   colnames(IndustryCodeName) <- c("Code", "Name")
   CommodityCodeName <- get(paste(model$specs$BaseIOLevel, "CommodityCodeName", model$specs$BaseIOSchema, sep = "_"))
   colnames(CommodityCodeName) <- c("Code", "Name")
-  # Modify IOtable row and column names based on table type
-  # Use "S00401 Scrap" or "Used" to determine rowname is Commodity or Industry
-  if ("S00401" %in% rownames(IOtable) == TRUE | "Used" %in% rownames(IOtable) == TRUE) {
+  # Determine whether it is Industry/Commodity in IOtable row/column
+  # Modify IOtable row and column names
+  if (unique(rownames(IOtable)%in%IndustryCodeName) == TRUE) {
+    # Row name is Industry
+    IndustryCodeName <- IndustryCodeName[IndustryCodeName$Code%in%rownames(IOtable), ]
+    IndustryCodeName <- IndustryCodeName[order(factor(IndustryCodeName$Code, levels = rownames(IOtable))), ]
+    rownames(IOtable) <- tolower(paste(rownames(IOtable), IndustryCodeName[, 2], "US", sep = "/"))
+  } else {
     # Row name is Commodity
     CommodityCodeName <- CommodityCodeName[CommodityCodeName$Code%in%rownames(IOtable), ]
     CommodityCodeName <- CommodityCodeName[order(factor(CommodityCodeName$Code, levels = rownames(IOtable))), ]
     rownames(IOtable) <- tolower(paste(rownames(IOtable), CommodityCodeName[, 2], "US", sep = "/"))
+  }
+  if (unique(colnames(IOtable)%in%IndustryCodeName) == TRUE) {
     # Column name is Industry
     IndustryCodeName <- IndustryCodeName[IndustryCodeName$Code%in%colnames(IOtable), ]
     IndustryCodeName <- IndustryCodeName[order(factor(IndustryCodeName$Code, levels = colnames(IOtable))), ]
     colnames(IOtable) <- tolower(paste(colnames(IOtable), IndustryCodeName[, 2], "US", sep = "/"))
   } else {
-    # Row name is Industry
-    IndustryCodeName <- IndustryCodeName[IndustryCodeName$Code%in%rownames(IOtable), ]
-    IndustryCodeName <- IndustryCodeName[order(factor(IndustryCodeName$Code, levels = rownames(IOtable))), ]
-    rownames(IOtable) <- tolower(paste(rownames(IOtable), IndustryCodeName[, 2], "US", sep = "/"))
     # Column name is Commodity
     CommodityCodeName <- CommodityCodeName[CommodityCodeName$Code%in%colnames(IOtable), ]
     CommodityCodeName <- CommodityCodeName[order(factor(CommodityCodeName$Code, levels = colnames(IOtable))), ]
     colnames(IOtable) <- tolower(paste(colnames(IOtable), CommodityCodeName[, 2], "US", sep = "/"))
-  }
+  } 
   # Need code here to format state IO tables
 
   return(IOtable)
