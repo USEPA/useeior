d9543e463db83aa86af1278150a8a7d9e4306dac
cga203 <65498439+cga203@users.noreply.github.com>
2020-06-22 17:18:45 -0400

    Update bio example for approach v02
    Update the example to be used when createBioeconomyModel() is called inside buildEEIOModel().

--- R/BuildModel.R
+++ R/BuildModel.R
@@ -72,22 +72,55 @@ prepareEEIOModel <- function(modelname) {
 buildEEIOModel <- function(modelname) {
   # Prepare model
   model <- prepareEEIOModel(modelname)
+  
+  #--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+  #Modify model for Bioeconomy new sectors 
+  #For now, just for adding 1 sector, then maybe a cycle must me used and maybe this cycle is written in another function
+  
+  #Info for new sector
+  newSectorCode<-"324110B"
+  newSectorName<-"LignoCelullosic Biofuels"
+  simSectorCode<-"324110"
+  percentage<- 0.01
+  # Obtain Input purchases from similar sector
+  #inputPurchases<- c(model$Use[1:405, simSectorCode],0)
+  inputPurchases<- c(rep(100,300),rep(0,106))
+  #colSim<-which(colnames(model2$Use)==simSectorCode)
+  #envVector<-model$B[,colSim] 
+  envVector<-rep(0,15) 
+  
+  #Modify model
+  #source("R/BioeconomyFunctions.R")
+  #debug(createBioeconomyModel)
+  model<-createBioeconomyModel(model,newSectorCode,newSectorName, simSectorCode,percentage, inputPurchases, envVector)
+  
+  #Note matrix B is modified below
+  #--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+  
   # Generate matrices
   model$V_n <- generateMarketSharesfromMake(model) # normalized Make
   model$U_n <- generateDirectRequirementsfromUse(model, domestic = FALSE) #normalized Use
-  model$U_d_n <- generateDirectRequirementsfromUse(model, domestic = TRUE) #normalized DomesticUse
+  # model$U_d_n <- generateDirectRequirementsfromUse(model, domestic = TRUE) #normalized DomesticUse
   model$W <- as.matrix(model$BEA$UseValueAdded)
+  
+  #--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+  # Update W matrix for Bioeconomy new sectors 
+  updatedUseValueAdded<- model$Use[model$BEA$ValueAddedCodes, model$Industries] * 1E6 # data frame, values are in dollars ($)
+  model$W <- as.matrix(updatedUseValueAdded)
+  
+  #--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+  
   if(model$specs$CommoditybyIndustryType == "Commodity") {
     logging::loginfo(paste("Building commodityxcommodity direct requirement matrix ..."))
     model$A <- model$U_n %*% model$V_n
-    model$A_d <- model$U_d_n %*% model$V_n
+  #  model$A_d <- model$U_d_n %*% model$V_n
   } else if(model$specs$CommoditybyIndustryType == "Industry") {
     logging::loginfo(paste("Building industryxindustry requirement matrix ..."))
     model$A <- model$V_n %*% model$U_n
     model$A_d <- model$V_n %*% model$U_d_n
   }
   # Create an imports requirements matrix 
-  model$A_m <- model$A - model$A_d
+  #model$A_m <- model$A - model$A_d
   # Generate satellite tables
   model$SatelliteTables <- loadsattables(model)
   # Combine satellite tables (coeffs_by_sector) into a single df
@@ -113,12 +146,23 @@ buildEEIOModel <- function(modelname) {
   # Generate B matrix
   model$B <- as.matrix(sattables_cast)
 
+  #--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+  # Modify B matrix for Bioeconomy new sectors 
+  
+  #Modify B matrix
+  logging::loginfo(paste("Updating B matrix ..."))
+  newB<- modifyBmatrix(newSectorCode,envVector, model$B, model$specs$PrimaryRegionAcronym)
+  model$B<-newB
+  
+  #--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+  
+  
   # Transform B into a flowxcommodity matrix using market shares matrix for commodity models
   if(model$specs$CommoditybyIndustryType == "Commodity") {
     model$B <- model$B %*% model$V_n
     colnames(model$B) <- tolower(paste(colnames(model$B), model$specs$PrimaryRegionAcronym, sep = "/"))
   }
-
+  
   # Generate C matrix: LCIA indicators
   factors_from_static <- loadindicators(model$specs)
   factors_from_static$GHG <- tolower(paste(factors_from_static$Name, factors_from_static$Category, factors_from_static$Subcategory,
@@ -134,27 +178,27 @@ buildEEIOModel <- function(modelname) {
   # Calculates total requirements matrix as Leontief inverse of A (L)
   logging::loginfo("Calculating total requirements matrix...")
   I <- diag(nrow(model$A))
-  I_d <- diag(nrow(model$A_d))
+  #I_d <- diag(nrow(model$A_d))
   model$L <- solve(I - model$A)
-  model$L_d <- solve(I_d - model$A_d)
+  #model$L_d <- solve(I_d - model$A_d)
   # Calculate total emissions/resource use per dollar (M)
   logging::loginfo("Calculating total emissions per dollar matrix...")
   model$M <- model$B %*% model$L
   colnames(model$M) <- tolower(paste(colnames(model$M), model$specs$PrimaryRegionAcronym, sep = "/"))
   #M_d are the domestic emissions per dollar using domestic Leontief
-  model$M_d <- model$B %*% model$L_d
-  colnames(model$M_d) <- tolower(paste(colnames(model$M_d), model$specs$PrimaryRegionAcronym, sep = "/"))
+  #model$M_d <- model$B %*% model$L_d
+  #colnames(model$M_d) <- tolower(paste(colnames(model$M_d), model$specs$PrimaryRegionAcronym, sep = "/"))
   #Calculate total requirements for imports - note different method
-  model$L_m <- model$A_m %*% model$L_d
+  #model$L_m <- model$A_m %*% model$L_d
   #M_e are the external emissions per dollar using the domestic technology assumption
-  model$M_e <- model$B %*% model$L_m
-  colnames(model$M_e) <- tolower(paste(colnames(model$M_e), model$specs$PrimaryRegionAcronym, sep = "/"))
+  #model$M_e <- model$B %*% model$L_m
+  #colnames(model$M_e) <- tolower(paste(colnames(model$M_e), model$specs$PrimaryRegionAcronym, sep = "/"))
   # Calculate total impacts per dollar (U), impact category x sector
   model$U <- model$C %*% model$M
   #U_d are the domestic impacts per dollar
-  model$U_d <- model$C %*% model$M_d
+  #model$U_d <- model$C %*% model$M_d
   #U_e are the external impacts per dollar using the domestic technology assumption
-  model$U_e <- model$C %*% model$M_e
+  #model$U_e <- model$C %*% model$M_e
   
   logging::loginfo("Model build complete.")
   return(model)
--- examples/USEEIO20-GHG-Bio.R
+++ examples/USEEIO20-GHG-Bio.R
@@ -7,36 +7,19 @@ devtools::install_github("modelearth/useeior")
 
 library(useeior)
 useeior::seeAvailableModels()
+
+# For now, take one existing sector and use its values.
+# This example with Biofuels
+# newSectorName: "LignoCelullosic Biofuels"
+# newSectorCode: "324110B"
+# similarSectorCode: "324110"- BEA code- Petroleum refineries
+# percentage: 1%
+# Input purchases from commodities: 100 for the first 300 commodities and 0 for the rest
+# New environmental data: 0 for all the environmental flows.
+# This is an example with the createBioeconomyModel() inside buildEEIOModel().
 model2 <- useeior::buildEEIOModel('USEEIOv2.0-GHG')
-#Define the input purchases vector sna newEnvironmentalDataVector
-#' For now, take one existing sector and use its values.
-#' This example with Biofuels
-#' newSectorName: "LignoCelullosic Biofuels"
-#' newSectorCode: "324110B"
-#' similarSectorCode: "324110"- BEA code- Petroleum refineries
-#' percentage: 20%
-#' Input purchases from commodities: Same as similar sector.
-#' New environmental data: Same as similar sector.
-
-# Obtain Input purchases from similar sector
-simSectorCode<-"324110"
-#inputPurchases<- c(model$Use[1:405, simSectorCode],0)
-inputPurchases<- c(rep(100,300),rep(0,106))
-colSim<-which(colnames(model2$Use)==simSectorCode)
-#envVector<-model$B[,colSim] 
-envVector<-rep(0,15) 
-#Modify model
-#source("R/BioeconomyFunctions.R")
-#debug(createBioeconomyModel)
-model2<-createBioeconomyModel(model2,newSectorCode="324110B",newSectorName="LignoCelullosic Biofuels", similarSectorCode=simSectorCode,percentage=0.01, inputPurchases, newEnvData=envVector)
-
 #Calculate model
 result2 <- useeior::calculateEEIOModel(model2, perspective='DIRECT')
-
-#for testing
-result2$LCIA_d["324110B",1]
-result2$LCIA_d["324110",1]
-result$LCIA_d["324110",1]
 
 #Print results
 # Everything is updated to use this function. The only "exception" is that we are not modifying the satellite tables directly, so we cannot update them but
