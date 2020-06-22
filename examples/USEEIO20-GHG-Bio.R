2f2c616fcd6febaff59bc4dfda319182922afeaa
cga203 <65498439+cga203@users.noreply.github.com>
2020-06-22 14:06:34 -0400

    Update bioeconomy example
    Where it imports the package and some other minor changes.

--- examples/USEEIO20-GHG-Bio.R
+++ examples/USEEIO20-GHG-Bio.R
@@ -1,4 +1,9 @@
 #This is an example of using the new Bioeconomy function
+#Start with clean
+remove.packages("useeior")
+#install
+devtools::install_github("modelearth/useeior")
+
 
 library(useeior)
 useeior::seeAvailableModels()
@@ -16,13 +21,15 @@ model2 <- useeior::buildEEIOModel('USEEIOv2.0-GHG')
 # Obtain Input purchases from similar sector
 simSectorCode<-"324110"
 #inputPurchases<- c(model$Use[1:405, simSectorCode],0)
-inputPurchases<- c(0.00001,rep(0,405))
+inputPurchases<- c(rep(100,300),rep(0,106))
 colSim<-which(colnames(model2$Use)==simSectorCode)
 #envVector<-model$B[,colSim] 
 envVector<-rep(0,15) 
 #Modify model
 source("R/BioeconomyFunctions.R")
-model2<-createBioeconomyModel(model2,newSectorCode="324110B",newSectorName="LignoCelullosic Biofuels", similarSectorCode=simSectorCode,percentage=0.00001, inputPurchases, newEnvData=envVector)
+#debug(createBioeconomyModel)
+model2<-createBioeconomyModel(model2,newSectorCode="324110B",newSectorName="LignoCelullosic Biofuels", similarSectorCode=simSectorCode,percentage=0.01, inputPurchases, newEnvData=envVector)
+
 #Calculate model
 result2 <- useeior::calculateEEIOModel(model2, perspective='DIRECT')
 
@@ -32,4 +39,6 @@ result2$LCIA_d["324110",1]
 result$LCIA_d["324110",1]
 
 #Print results
+# Everything is updated to use this function. The only "exception" is that we are not modifying the satellite tables directly, so we cannot update them but
+# in consequence, the file that ends in "_sat.csv" doesn't make sense.
 useeior::writeModelComponents(model2)
