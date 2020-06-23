9b29d011b8f69c3aa728859ca8c834ef19cf0da7
cga203 <65498439+cga203@users.noreply.github.com>
2020-06-23 10:46:01 -0400

    Update USEEIO20-GHG-Bio.R
    Update example for v03 where we only need to run createBioeconomyModel() and calculateEEIOModel().

--- examples/USEEIO20-GHG-Bio.R
+++ examples/USEEIO20-GHG-Bio.R
@@ -8,7 +8,7 @@ devtools::install_github("modelearth/useeior")
 library(useeior)
 useeior::seeAvailableModels()
 
-# For now, take one existing sector and use its values.
+
 # This example with Biofuels
 # newSectorName: "LignoCelullosic Biofuels"
 # newSectorCode: "324110B"
@@ -16,11 +16,30 @@ useeior::seeAvailableModels()
 # percentage: 1%
 # Input purchases from commodities: 100 for the first 300 commodities and 0 for the rest
 # New environmental data: 0 for all the environmental flows.
-# This is an example with the createBioeconomyModel() inside buildEEIOModel().
-model2 <- useeior::buildEEIOModel('USEEIOv2.0-GHG')
+
+
+#Info for new sector
+newSectorCode<-"324110B"
+newSectorName<-"LignoCelullosic Biofuels"
+simSectorCode<-"324110"
+percentage<- 0.01
+# Obtain Input purchases from similar sector
+#inputPurchases<- c(model$Use[1:405, simSectorCode],0)
+inputPurchases<- c(rep(0,300),rep(0,106))
+#colSim<-which(colnames(model2$Use)==simSectorCode)
+#envVector<-model$B[,colSim] 
+envVector<-rep(0,15) 
+
+
+#Create model
+model2<-createBioeconomyModel('USEEIOv2.0-GHG',newSectorCode,newSectorName, simSectorCode,percentage, inputPurchases, envVector)
 
 #Calculate model
 result2 <- useeior::calculateEEIOModel(model2, perspective='DIRECT')
+
+#See some results
+result2$LCIA_d["324110B",1]
+result2$LCIA_d["324110",1]
 
 #Print results
 # Everything is updated to use this function. The only "exception" is that we are not modifying the satellite tables directly, so we cannot update them but
