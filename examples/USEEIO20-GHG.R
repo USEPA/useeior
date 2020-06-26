46b5ca4078febc17d8d802a94767058f27a003a2
MoLi7 <mo.vico.li@gmail.com>
2020-06-26 11:01:34 -0700

    Minor revisions in example script

--- examples/USEEIO20-GHG.R
+++ examples/USEEIO20-GHG.R
@@ -2,13 +2,15 @@
 remove.packages("useeior")
 #install
 devtools::install_github("USEPA/useeior")
+# See https://github.com/USEPA/useeior for more information.
 
 library(useeior)
 useeior::seeAvailableModels()
 model <- useeior::buildEEIOModel('USEEIOv2.0-GHG')
+# Run ?calculateEEIOModel to see documentation for calculateEEIOModel()
 result <- useeior::calculateEEIOModel(model, perspective='DIRECT')
 useeior::writeModelComponents(model)
-useeior::writeModelMatrices(model)
+#useeior::writeModelMatrices(model)
 useeior::writeModelMatricesforAPI(model)
 useeior::writeModelDemandstoJSON(model)
 useeior::writeModelMetadata(model)
