c458a440339dfe963c3d726147880f476548e50a
catherinebirney <60186515+catherinebirney@users.noreply.github.com>
2020-06-29 17:38:01 -0400

    update documentation in example file

--- examples/USEEIO20-GHG.R
+++ examples/USEEIO20-GHG.R
@@ -1,14 +1,19 @@
-#Start with clean
+# Example for running useeior, see https://github.com/USEPA/useeior for more information on the model
+# Run ?functionname to see documentation and function input options for the functions in this example
+# The output csv files are the inputs for useeiopy (https://github.com/USEPA/useeiopy)
+
+# Start clean by removing useeior package and installing latest version
 remove.packages("useeior")
-#install
 devtools::install_github("USEPA/useeior")
-# See https://github.com/USEPA/useeior for more information.
 
 library(useeior)
+
+# See the versioning scheme for explanation of model names (https://github.com/USEPA/USEEIO/blob/master/VersioningScheme.md)
 useeior::seeAvailableModels()
 model <- useeior::buildEEIOModel('USEEIOv2.0-GHG')
-# Run ?calculateEEIOModel to see documentation for calculateEEIOModel()
 result <- useeior::calculateEEIOModel(model, perspective='DIRECT')
+
+# output model results in various formats
 useeior::writeModelComponents(model)
 #useeior::writeModelMatrices(model)
 useeior::writeModelMatricesforAPI(model)
