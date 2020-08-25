944ddf7d3d8ac807b8d40f55a981969580fbd361
WesIngwersen <ingwersen.wesley@epa.gov>
2020-08-25 16:45:43 -0400

    -Adds loadandbuildSatellitesTables - major changes to loadsattables to enable dynamic modeling

--- examples/USEEIO20-GHG.R
+++ examples/USEEIO20-GHG.R
@@ -11,6 +11,7 @@ library(useeior)
 # See the versioning scheme for explanation of model names (https://github.com/USEPA/USEEIO/blob/master/VersioningScheme.md)
 useeior::seeAvailableModels()
 model <- useeior::loadIOData('USEEIOv2.0-GHG')
+model <- loadbuildSatelliteTables(model)
 model <- useeior::buildEEIOModel(model)
 result <- useeior::calculateEEIOModel(model, perspective='DIRECT')
 
