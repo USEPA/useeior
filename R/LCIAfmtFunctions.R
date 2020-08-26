aa09bc768da6c215e8ebc76a9e6b23c5a52044e0
WesIngwersen <ingwersen.wesley@epa.gov>
2020-08-26 08:51:03 -0400

    Modify getInventoryMethod to getInventoryMethods without subset call due to reticulate NoneType error, and then use subset to filter results of R df

--- R/LCIAfmtFunctions.R
+++ R/LCIAfmtFunctions.R
@@ -2,9 +2,10 @@
 #' Calls the Python flowsa package's getFlowBySector method
 #' @param method_name The name
 #' @return A dataframe for flowsa data in sector by region totals format
-getInventoryMethod <- function(subset=subset) {
+getInventoryMethod <- function(subset) {
   lciafmt <- reticulate::import("lciafmt")
-  inv_method <- lciafmt$get_method(method_id="FEDEFL Inventory",subset)
+  inv_method <- lciafmt$get_method(method_id="FEDEFL Inventory")
+  inv_method <- inv_method[inv_method['Indicator']==subset,]
   return(inv_method)
 }
   
