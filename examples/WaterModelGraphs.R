00bf5c9732ce3bd78c150b338e37417be18c821f
MoLi7 <mo.vico.li@gmail.com>
2020-09-05 11:39:49 -0700

    Apply barplotFlowbyGroup to draw total water use (fresh and saline) by 2-digit NAICS

--- examples/WaterModelGraphs.R
+++ examples/WaterModelGraphs.R
@@ -22,3 +22,12 @@ p <- lineplotFlowCoefficients(flow, matrix_list, modelspecs_list) +
 
 plot(p)
 
+# Total water (fresh and saline) by 2-digit NAICS
+methodname <- "Water_national_2015_m1"
+fresh_water <- c("resource/water", "resource/water/fresh water body", "resource/water/subterranean/fresh water body")
+saline_water <- c("resource/water/saline water body", "resource/water/subterranean/saline water body")
+group <- list("Fresh Water" = fresh_water, "Saline Water" = saline_water)
+
+p <- barplotFlowbySector(methodname, NAICS_digit = 2, group)
+
+plot(p)
