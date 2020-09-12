d3e7311b226b55a0490cd87617b78a82303a515b
WesIngwersen <ingwersen.wesley@epa.gov>
2020-09-12 13:50:27 -0400

    revise lineplot name in namespace

--- R/VisualizationFunctions.R
+++ R/VisualizationFunctions.R
@@ -7,34 +7,31 @@
 #' @param coefficient_name Row name in the specified matrix for the line plot
 #' @export
 lineplotMatrixCoefficient <- function(model_list,matrix_name,coefficient_name) {
-  colormap <- getBEASectorColorMapping()
+  mapping <- getBEASectorColorMapping(model_list[[1]]$specs$BaseIOLevel)
   
   df <- data.frame()
-  for (name in names(matrix_list)) {
+  for (modelname in names(model_list)) {
     df_model <- data.frame()
-    matrix <- matrix_list[[name]]
-    modelspecs <- modelspecs_list[[name]]
-    matrix <- colSums(matrix[rownames(matrix)==flow, , drop = FALSE])
+    model <- model_list[[modelname]]
+    matrix <- model[[matrix_name]]
+    modelspecs <- model$specs
+    matrix <- matrix[rownames(matrix)==coefficient_name,]
     matrix <- cbind.data.frame(names(matrix), matrix)
     colnames(matrix) <- c("SectorCode", "Coeff")
     matrix$SectorCode <- toupper(gsub("/.*", "", matrix$SectorCode))
     df_model <- rbind(matrix, df_model)
-    #mapping <- mapping[, c(modelspecs$BaseIOLevel, "Sector")]
-    #mapping <- unique(mapping)
-    df_model <- merge(df_model, mapping, by.x = "SectorCode", by.y = modelspecs$BaseIOLevel)
-    #df_model <- merge(df_model, ColorLabelMapping, by.x = "Sector", by.y = "V2")
-    df_model <- merge(df_model, modelspecs$SectorCodeName, by = "SectorCode")
-    df_model$modelname <- name
+    df_model <- merge(df_model, mapping, by.x = "SectorCode", by.y = paste0(model$specs$BaseIOLevel,"Code"))
+    df_model$modelname <- modelname
     df <- rbind(df, df_model)
   }
   #! Temp unit hardcoding - should come from flow
   y_unit <- "(kg/$)"
   # plot
-  p <- ggplot2::ggplot(df, ggplot2::aes(x = factor(SectorCode, levels = modelspecs$SectorCodeName$SectorCode),
+  p <- ggplot2::ggplot(df, ggplot2::aes(x = SectorCode,
                                         y = Coeff, group = as.character(modelname))) +
     ggplot2::geom_line() + ggplot2::aes(color = as.character(modelname)) +
     #scale_color_manual(values = c('#e41a1c','#377eb8','#4daf4a','#984ea3','#ff7f00')) +
-    ggplot2::labs(x = "", y = paste(tools::toTitleCase(flow), "Factors", y_unit)) +
+    ggplot2::labs(x = "", y = paste(coefficient_name, y_unit)) +
     ggplot2::scale_x_discrete(breaks = df$SectorCode, labels = df$SectorName) +
     ggplot2::scale_y_continuous(expand = c(0, 0)) + ggplot2::coord_cartesian() + #ylim = ylim # removed from coord_cartesian
     ggplot2::theme_linedraw(base_size = 15) +
@@ -75,13 +72,10 @@ barplotIndicatorScoresbySector <- function(model_list, totals_by_sector_name, in
 
   }
   
-
-  
-  
   # Plot
-  p <- ggplot2::ggplot(df, ggplot2::aes(x = Model, y = IndicatorScore, fill = factor(SectorName, levels = mapping$SectorName))) +
+  p <- ggplot2::ggplot(df, ggplot2::aes(x = Model, y = IndicatorScore, fill = SectorName)) +
     ggplot2::geom_bar(stat = "identity") + ggplot2::scale_fill_manual(values = mapping$color) +
-    ggplot2::labs(x = "", y = paste0("Indicator Score of ", indicator_code, " (", Unit, ")")) +
+    ggplot2::labs(x = "", y = paste0(indicator_code, " (", Unit, ")")) +
     ggplot2::scale_y_continuous(expand = c(0, 0), labels = function(x) format(x, scientific = TRUE)) +
     ggplot2::theme_linedraw(base_size = 15) +
     ggplot2::theme(axis.text = ggplot2::element_text(color = "black", size = 15),
@@ -97,6 +91,10 @@ barplotIndicatorScoresbySector <- function(model_list, totals_by_sector_name, in
 
 
 ## Helper functions for plotting
+
+#' Uses VizualizationEssentials.yml to get a mapping of the to the BEA Sector Color scheme
+#' @param BaseIOLevel The model$BaseIOLevel to use
+#' @return df with mapping with model BaseIOLevel codes to BEA Sector Codes, Names, and colors
 getBEASectorColorMapping <- function(BaseIOLevel){
   configfile <- system.file("extdata", "VisualizationEssentials.yml", package="useeior")
   VisualizationEssentials <- configr::read.config(configfile)
