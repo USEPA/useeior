54ee04d742bdd0acb7604e03239fdb798cbea231
MoLi7 <mo.vico.li@gmail.com>
2020-10-09 14:48:38 -0700

    Update DataQuality.R

--- R/DataQuality.R
+++ R/DataQuality.R
@@ -1,8 +1,8 @@
-#'Draws from https://github.com/USEPA/ElectricityLCI/blob/master/electricitylci/dqi.py 
+#' Draws from https://github.com/USEPA/ElectricityLCI/blob/master/electricitylci/dqi.py 
 
-#'Get DQ fields in existing data frame
-#'@param df totals data frame 
-#'@return string vector with names of data quality fields
+#' Get DQ fields in existing data frame
+#' @param df A totals_by_sector data frame
+#' @return A string vector with names of data quality fields
 getDQfields <- function (df) {
   flow_data_quality_fields <- c("DataReliability",
                                 "TemporalCorrelation",
@@ -15,7 +15,7 @@ getDQfields <- function (df) {
 
 #' Constants to determine which bound type to use for DQ indicator
 #' and for each indicator, provide those bounds
-#' @return a list with DQ bounds
+#' @return A list with DQ bounds
 setDQScoringBounds <- function() {
   bound_to_dqi <- list()
   bound_to_dqi[["upper"]] <- c("TemporalCorrelation")
@@ -58,15 +58,15 @@ lookupDQBoundScore <- function(raw_score,dqi,scoring_bounds) {
     }
     if (is.na(score)) score<-5
   } else {
-    logging::logerror(paste0("No bounds defined for ",dqi))
+    logging::logerror(paste("No bounds defined for", dqi))
   }
   return(score)
   
 }
 
 #' Adds contextual data quality scores to a data frame with Year present
-#'@param df a dataframe containing a 'Year' column representing data year
-#'@return a dataframe with contextual data quality scores added in columns
+#' @param df A data frame containing a 'Year' column representing data year
+#' @return A data frame with contextual data quality scores added in columns
 #' with names of the indicators. Only 'TemporalCorrelation' currently added.
 scoreContextualDQ <- function(df)  {
   bounds <- setDQScoringBounds()    
@@ -75,11 +75,11 @@ scoreContextualDQ <- function(df)  {
   return(df)
 }
 
-#'Scores temporal data quality using a lookup based on difference between data year and target year
-#'@param data_year integer year of data
-#'@param target_year integer year of data, defaults to current year
-#'@param scoring_bounds global scoring bounds
-#'@return an integer data quality score 1-5 or NA
+#' Scores temporal data quality using a lookup based on difference between data year and target year
+#' @param data_year integer year of data
+#' @param target_year integer year of data, defaults to current year
+#' @param scoring_bounds global scoring bounds
+#' @return An integer data quality score 1-5 or NA
 scoreTemporalDQ <- function(data_year,target_year=NA, scoring_bounds) {
   if (is.na(data_year)) {
     return(NA)
@@ -87,7 +87,7 @@ scoreTemporalDQ <- function(data_year,target_year=NA, scoring_bounds) {
   if (is.na(target_year)) {
     target_year <- as.integer(format(Sys.Date(), "%Y"))
   }
-  age <- target_year-data_year
-  score <- lookupDQBoundScore(age,"TemporalCorrelation",scoring_bounds) 
+  age <- target_year - data_year
+  score <- lookupDQBoundScore(age, "TemporalCorrelation", scoring_bounds) 
   return(score)
 }
