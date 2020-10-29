4f358acbecb50046483a8e84f65d82c1c330dfee
MoLi7 <mo.vico.li@gmail.com>
2020-10-29 11:37:56 -0700

    Minor fixes in DemandFunctions.R

--- R/DemandFunctions.R
+++ R/DemandFunctions.R
@@ -7,11 +7,11 @@ getUSTotalConsProd <- function (model) {
                                  as.matrix(rowSums(model$FinalDemand)))
   colnames(USConsProd) <- paste(model$specs$IOYear, c("_US_Consumption", "_US_Production"), sep = "")
   # Merge USConsProd with model$SectorNames
-  USConsProd <- merge(model$SectorNames, USConsProd, by.x = "SectorCode", by.y = 0)
+  USConsProd <- merge(model$SectorNames, USConsProd, by.x = "Sector", by.y = 0)
   ConsProdcolumns <- paste(model$specs$IOYear, "US", c("Consumption", "Production"), sep = "_")
   # Add Location column, re-order columns (drop useless columns), and modify colnames
   USConsProd$Location <- "US"
-  USTotalConsProd <- USConsProd[c("SectorCode", "SectorName", "Location", ConsProdcolumns)]
+  USTotalConsProd <- USConsProd[c("Sector", "SectorName", "Location", ConsProdcolumns)]
   colnames(USTotalConsProd)[1:2] <- c("BEA_Code", "BEA_Name")
   # # Remove Scrap row if ScrapIncluded is TRUE in model configuration
   # if (model$specs$ScrapIncluded == TRUE) {
