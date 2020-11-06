4ad79fa4109d9558c433f85638a82c03e596a0e0
Ben Young <44471635+bl-young@users.noreply.github.com>
2020-11-06 16:56:51 -0500

    Keep only the IO schema needed for crosswalk. apply disaggregation of crosswalk

--- R/LoadIOTables.R
+++ R/LoadIOTables.R
@@ -9,10 +9,11 @@ loadIOData <- function(modelname) {
   # Get model specs
   model$specs <- getModelConfiguration(modelname)
   # Get model crosswalk
-  if (is.null(model$specs$DisaggregationSpecs)) {
-    model$crosswalk <- get(paste0("MasterCrosswalk", model$specs$BaseIOSchema))
-  } else {
-    # add crosswalk of disaggregated sectors here
+  model$crosswalk <- get(paste0("MasterCrosswalk", model$specs$BaseIOSchema))
+  model$crosswalk <- unique(model$crosswalk[, c("NAICS_2012_Code", paste("BEA", model$specs$BaseIOSchema,model$specs$BaseIOLevel, "Code", sep = "_"))])
+  colnames(model$crosswalk) <- c("NAICS", "BEA")
+  if(!is.null(model$specs$DisaggregationSpecs)){
+    model$crosswalk <- disaggregateMasterCrosswalk(model$crosswalk)
   }
   # Get BEA IO tables
   model$BEA <- loadBEAtables(model$specs)
