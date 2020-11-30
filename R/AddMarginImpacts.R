0698dc6f78acc3b7ab5f22e0dca05ca274a4be2f
WesIngwersen <ingwersen.wesley@epa.gov>
2020-11-30 07:41:42 -0500

    Revise nomenclature to match updated proposed nomenclature

--- R/AddMarginImpacts.R
+++ R/AddMarginImpacts.R
@@ -2,7 +2,7 @@
 #' @param model A complete EEIO model: a list with USEEIO model components and attributes.
 #' @param margin_type A character value: can be "intermediate" or "final consumer".
 #' @export
-#' @return A list with M_margin and U_margin
+#' @return A list with M_m and U_m
 deriveMarginSectorImpacts <- function(model, margin_type = "intermediate") {
   # Determine Margins table
   if (margin_type=="intermediate") {
@@ -12,36 +12,36 @@ deriveMarginSectorImpacts <- function(model, margin_type = "intermediate") {
   }
   ## Add in impacts of margin sectors
   # Calculation fractions of producer price for each margin
-  MarginCoefficients <- as.matrix(Margins[, c("Transportation", "Wholesale", "Retail")]/Margins[, c("ProducersValue")])
-  rownames(MarginCoefficients) <- Margins$CommodityCode
+  Phi <- as.matrix(Margins[, c("Transportation", "Wholesale", "Retail")]/Margins[, c("ProducersValue")])
+  rownames(Phi) <- Margins$SectorCode
   
-  # Create margin_allocation matrix to allocate fractions by margin sector
+  # Create margin_allocation matrix, MA, to allocate fractions by margin sector
   # In the matrix, rows are three margin types and columns are margin sectors
   all_margin_sectors <- c(model$BEA$TransportationCodes, model$BEA$WholesaleCodes, model$BEA$RetailCodes)
-  margin_allocation <- matrix(nrow = 3, ncol = length(all_margin_sectors), 0)
-  rownames(margin_allocation) <- colnames(MarginCoefficients)
-  colnames(margin_allocation) <- all_margin_sectors
+  MA <- matrix(nrow = 3, ncol = length(all_margin_sectors), 0)
+  rownames(MA) <- colnames(Phi)
+  colnames(MA) <- all_margin_sectors
   # Assign allocation factors to margin sectors based on total Commodity output
   output_ratio <- calculateOutputRatio(model, output_type="Commodity")
-  margin_allocation["Transportation", model$BEA$TransportationCodes] <- output_ratio[output_ratio$SectorCode%in%model$BEA$TransportationCodes, "toSectorRatio"]
-  margin_allocation["Wholesale", model$BEA$WholesaleCodes] <- output_ratio[output_ratio$SectorCode%in%model$BEA$WholesaleCodes, "toSectorRatio"]
-  margin_allocation["Retail", model$BEA$RetailCodes] <- output_ratio[output_ratio$SectorCode%in%model$BEA$RetailCodes, "toSectorRatio"]
+  MA["Transportation", model$BEA$TransportationCodes] <- output_ratio[output_ratio$SectorCode%in%model$BEA$TransportationCodes, "toSectorRatio"]
+  MA["Wholesale", model$BEA$WholesaleCodes] <- output_ratio[output_ratio$SectorCode%in%model$BEA$WholesaleCodes, "toSectorRatio"]
+  MA["Retail", model$BEA$RetailCodes] <- output_ratio[output_ratio$SectorCode%in%model$BEA$RetailCodes, "toSectorRatio"]
   
   # Multiply fractions by allocation matrix to get a fraction per margin sector for each commodity
-  margins_by_sector <- MarginCoefficients %*% margin_allocation
+  Phi_m <- Phi %*% MA
   
-  # Put margins_by_sector into a matrix in the form of A
-  A_margin <- model$A
+  # Put Phi_m into a matrix in the form of A
+  A_m <- model$A
   # Make sure sector ordering is the same
-  A_margin[,] <- 0 
+  A_m[,] <- 0 
   for (s in all_margin_sectors) {
-    A_margin[s, ] <- margins_by_sector[, s]
+    A_m[s, ] <- Phi_m[, s]
   }
-  # Multiply M and U by margins_by_sector to derive M_margin and U_margin
-  model$M_margin <- model$M %*% A_margin
-  colnames(model$M_margin) <- tolower(paste(colnames(model$M_margin), model$specs$PrimaryRegionAcronym, sep = "/"))
-  model$U_margin <- model$U %*% A_margin
-  colnames(model$U_margin) <- tolower(paste(colnames(model$U_margin), model$specs$PrimaryRegionAcronym, sep = "/"))
+  # Multiply M and U by Phi_m to derive M_m and U_m
+  model$M_m <- model$M %*% A_m
+  colnames(model$M_m) <- tolower(paste(colnames(model$M_m), model$specs$PrimaryRegionAcronym, sep = "/"))
+  model$U_m <- model$U %*% A_m
+  colnames(model$U_m) <- tolower(paste(colnames(model$U_m), model$specs$PrimaryRegionAcronym, sep = "/"))
   logging::loginfo("Model margin impacts derived")
   return(model)
 }
