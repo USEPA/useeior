2ab90de1123241eabe04b175bcdcc6b895d8d3d2
Ben Young <44471635+bl-young@users.noreply.github.com>
2020-12-29 14:05:10 -0500

    Use BEA based industry list when grabbing BEA data

--- R/SatelliteFunctions.R
+++ R/SatelliteFunctions.R
@@ -168,7 +168,7 @@ calculateIndicatorScoresforTotalsBySector <- function(model, totals_by_sector_na
 #' @return A value-added totals_by_sector table with fields of standard totals_by_sector
 getValueAddedTotalsbySector <- function(model) {
   # Extract ValueAdded from Use table
-  df <- model$Use[model$BEA$ValueAddedCodes, model$Industries] * 1E6 # data frame, values are in dollars ($)
+  df <- model$Use[model$BEA$ValueAddedCodes, model$BEA$Industries] * 1E6 # data frame, values are in dollars ($)
   # Sum ValueAdded
   df <- as.data.frame(colSums(df))
   # Add columns to convert to standard totals_by_sector format
