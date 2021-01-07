8dbb21e814be1ba2a59a79a7e9c8f0f8400b29f1
WesIngwersen <ingwersen.wesley@epa.gov>
2021-01-07 13:51:08 -0500

    Document all demand functions

--- R/DemandFunctions.R
+++ R/DemandFunctions.R
@@ -1,4 +1,4 @@
-#' Register functions for preset demand vector
+# Register functions for preset demand vector
 dem_vec_fxn_registry <- list()
 dem_vec_fxn_registry["Consumption"] <- "prepareConsumptionDemand"
 dem_vec_fxn_registry["Production"] <- "prepareProductionDemand"
@@ -12,7 +12,9 @@ dem_vec_fxn_registry["Household"] <- "prepareHouseholdDemand"
 
 
 #'Sums across sectors for a given set of BEA codes/cols in a given final demand df
-#'
+#'@param Y, a model Demand df 
+#'@param codes, sector code(s) for a subset of Final Demand cols
+#'@return a named vector with model sectors and demand amounts
 sumDemandCols <- function(Y,codes) {
   if (length(codes)>1) {
     y <- rowSums(Y[,codes])
@@ -23,6 +25,9 @@ sumDemandCols <- function(Y,codes) {
   return(y)
 }
 
+#'Sums the demand cols representing final consumption
+#'@param Y, a model Demand df 
+#'@return a named vector with model sectors and demand amounts
 sumforConsumption <- function(Y) {
   Y_h <- sumDemandCols(Y,model$BEA$HouseholdDemandCodes)
   Y_v <- sumDemandCols(Y,model$BEA$InvestmentDemandCodes) 
@@ -31,6 +36,10 @@ sumforConsumption <- function(Y) {
   return (y_c) 
 }
 
+
+#'Prepares a demand vector representing production
+#'@param model, a model
+#'@return a named vector with demand
 prepareProductionDemand <- function(model) {
   y_dc <- sumforConsumption(model$DomesticFinalDemand)
   Y <- model$FinalDemand
@@ -40,12 +49,50 @@ prepareProductionDemand <- function(model) {
   return(y_p)
 }
 
+#'Prepares a demand vector representing household consumption
+#'@param model, a model
+#'@return a named vector with demand
 prepareConsumptionDemand <- function(model) {
   y_c <- sumforConsumption(model$FinalDemand)
   return(y_c)
 }
 
+#'Prepares a demand vector representing consumption
+#'@param model, a model
+#'@return a named vector with demand
 prepareHouseholdDemand <- function(model) {
   y_h <- sumDemandCols(Y,model$BEA$HouseholdDemandCodes)
   return(y_h)
 }
+
+#'A function to validate a user provided demand vector
+#' @param dv a user provided demand vector
+#' @param L, the L matrix for the given model, used as a reference
+#' 
+isDemandVectorValid <- function(dv,L){
+  #should be a format like this
+  # >dv <- c("1111A0"=1,"1111B0"=2,"327100"=30)
+  # > dv
+  #1111A0 1111B0 327100 
+  #1      2     30 
+  
+  
+  #!temp just return true
+  return(TRUE)
+}
+
+#' Format a named demand vector with partial sectors to have all the rows and ordering needed
+#' @param dv, a user provided demand vector. See calculateEEIOModel()
+#' @param L, the L matrix for the given model, used as a reference
+#' @return a named vector with values for all names in L and ordered like L
+formatDemandVector <- function(dv,L) {
+  #create a named vector using the first col of L
+  d <- c(L[,1])
+  #Set all values to 0
+  d[0:nrow(L)] <- 0
+  #merge in names from dv
+  #replace the values from d
+  d[match(names(dv),names(d))] <- dv
+  return(d)
+}
+
