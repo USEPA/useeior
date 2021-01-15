7a087d7935607fcc74b4b611597ea88eff93963d
WesIngwersen <ingwersen.wesley@epa.gov>
2021-01-15 07:32:31 -0500

    Separate out initial logic in processing sats in specs into generateTbSfromSatSpec and conformTbStoIOSchema functions

--- R/LoadSatellites.R
+++ R/LoadSatellites.R
@@ -64,51 +64,11 @@ loadSatTables <- function(model) {
 
   for (sat in model$specs$SatelliteTable) {
     logging::loginfo(paste("Adding", tolower(sat$FullName), "satellite tables..."))
-    ### Generate totals_by_sector
-    # Check if the satellite table uses a static file. If so, proceed.
-    # If not, use specified functions in model metadata to load data from dynamic source
-    if(!is.null(sat$StaticFile)) {
-      # If the file is a URL tested by the first 4 characters of the string = "http", don't wrap in system.file()
-      if (substring(sat$StaticFile, 0, 4)=="http") {
-        totals_by_sector <- utils::read.table(sat$StaticFile, sep = ",", header = TRUE, stringsAsFactors = FALSE,
-                                              fileEncoding = 'UTF-8-BOM')  
-      } else {
-        totals_by_sector <- utils::read.table(system.file("extdata", sat$StaticFile, package = "useeior"),
-                                              sep = ",", header = TRUE, stringsAsFactors = FALSE,
-                                              fileEncoding = 'UTF-8-BOM')
-      }
-    } else {
-      func_to_eval <- sat$ScriptFunctionCall
-      totalsgenfunction <- as.name(func_to_eval)
-      if (sat$ScriptFunctionParameters == "model") {
-        params <- model
-      } else {
-        params <- sat$ScriptFunctionParameters
-      }
-      totals_by_sector <- do.call(eval(totalsgenfunction), list(params))
-    }
-    # Check if the orginal data is BEA-based. If so, apply necessary allocation or aggregation.
-    # If not, map data from original sector to BEA.
-    if (sat$SectorListSource == "BEA") {
-      # If BEA years is not the same as model year, must perform allocation
-      if (sat$SectorListLevel == "Detail" && sat$SectorListYear == 2007 && model$specs$BaseIOSchema == 2012) {
-        totals_by_sector <- mapFlowTotalsbySectorfromBEASchema2007to2012(totals_by_sector)
-      }
-      # If the orginal data is at Detail level but model is not, apply aggregation
-      if (sat$SectorListLevel == "Detail" && model$specs$BaseIOLevel != "Detail") {
-        totals_by_sector <- aggregateSatelliteTable(totals_by_sector,
-                                                    from_level = sat$SectorListLevel,
-                                                    to_level = model$specs$BaseIOLevel,
-                                                    model)
-      }
-    } else if ("NAICS" %in% sat$SectorListSource) {
-      totals_by_sector <- mapFlowTotalsbySectorandLocationfromNAICStoBEA(totals_by_sector, sat$DataYears[1], model)
-    }
-
-    # Check if disaggregation is needed based on model metadata
-    if(!is.null(model$specs$DisaggregationSpecs) & !is.null(sat$StaticFile)){
-      totals_by_sector <- disaggregateSatelliteTable(model, totals_by_sector, sat)
-    }
+    ### Generate totals_by_sector, tbs
+    totals_by_sector <- generateTbSfromSatSpec(sat)
+    
+    ### Make tbs conform to the model schema
+    totals_by_sector <- conformTbStoIOSchema(sat, model)
     
     # Add in DQ columns and additional contextual scores not provided
     # Only setting TemporalCorrelation for now
@@ -118,11 +78,17 @@ loadSatTables <- function(model) {
     if(len_dq_fields!=5){
       logging::logerror(paste("Missing 1 or more data quality fields in satellite data.", len_dq_fields, "present"))
     }
+    
     # Convert totals_by_sector to standard satellite table format
     totals_by_sector <- generateStandardSatelliteTable(totals_by_sector)
     
-    ### Generate coeffs_by_sector
-    coeffs_by_sector <- data.frame()
+    #Map names for static files not already using FEDEFL
+    if (!is.null(sat$StaticFile)) {
+      if (!substring(sat$OriginalFlowSource,1,6) == 'FEDEFL') {
+        totals_by_sector <- mapListbyName(totals_by_sector, sat)
+      }
+    }
+    
     for (r in model$specs$ModelRegionAcronyms) {
       sattable_r <- totals_by_sector[totals_by_sector$Location==r, ]
       if (r=="RoUS") {
@@ -140,12 +106,7 @@ loadSatTables <- function(model) {
       coeffs_by_sector <- rbind(coeffs_by_sector, coeffs_by_sector_r)
       }
     }
-    # If the satellite table uses a static file, it will use the embedded mapping files to map flows to internal flow names
-    if (!is.null(sat$StaticFile)) {
-      if (!substring(sat$OriginalFlowSource,1,6) == 'FEDEFL') {
-      coeffs_by_sector <- mapListbyName(coeffs_by_sector, sat)
-      }
-    }
+
     
     # Add totals_by_sector and coeffs_by_sector to the sattables list
     sattables$totals_by_sector[[sat$Abbreviation]] <- totals_by_sector
@@ -185,4 +146,56 @@ standardizeandcastSatelliteTable <- function(sattable,model) {
   # Adjust column order to be the same with V_n rownames
   sattables_cast <- sattables_cast[, standard_columns]
   return(sattables_cast)
+}
+
+
+generateTbSfromSatSpec <- function(sat_spec) {
+  # Check if the satellite table uses a static file. If so, proceed.
+  # If not, use specified functions in model metadata to load data from dynamic source
+  if(!is.null(sat_spec$StaticFile)) {
+    # If the file is a URL tested by the first 4 characters of the string = "http", don't wrap in system.file()
+    if (substring(sat_spec$StaticFile, 0, 4)=="http") {
+      totals_by_sector <- utils::read.table(sat_spec$StaticFile, sep = ",", header = TRUE, stringsAsFactors = FALSE,
+                                            fileEncoding = 'UTF-8-BOM')  
+    } else {
+      totals_by_sector <- utils::read.table(system.file("extdata", sat_spec$StaticFile, package = "useeior"),
+                                            sep = ",", header = TRUE, stringsAsFactors = FALSE,
+                                            fileEncoding = 'UTF-8-BOM')
+    }
+  } else {
+    func_to_eval <- sat_spec$ScriptFunctionCall
+    totalsgenfunction <- as.name(func_to_eval)
+    if (sat_spec$ScriptFunctionParameters == "model") {
+      params <- model
+    } else {
+      params <- sat_spec$ScriptFunctionParameters
+    }
+    totals_by_sector <- do.call(eval(totalsgenfunction), list(params))
+  }
+  return(totals_by_sector)
+}
+
+conformTbStoIOSchema <- function(sat_spec, model) {
+  # Check if the orginal data is BEA-based. If so, apply necessary allocation or aggregation.
+  # If not, map data from original sector to BEA.
+  if (sat_spec$SectorListSource == "BEA") {
+    # If BEA years is not the same as model year, must perform allocation
+    if (sat_spec$SectorListLevel == "Detail" && sat_spec$SectorListYear == 2007 && model$specs$BaseIOSchema == 2012) {
+      totals_by_sector <- mapFlowTotalsbySectorfromBEASchema2007to2012(totals_by_sector)
+    }
+    # If the orginal data is at Detail level but model is not, apply aggregation
+    if (sat_spec$SectorListLevel == "Detail" && model$specs$BaseIOLevel != "Detail") {
+      totals_by_sector <- aggregateSatelliteTable(totals_by_sector,
+                                                  from_level = sat_spec$SectorListLevel,
+                                                  to_level = model$specs$BaseIOLevel,
+                                                  model)
+    }
+  } else if ("NAICS" %in% sat_spec$SectorListSource) {
+    totals_by_sector <- mapFlowTotalsbySectorandLocationfromNAICStoBEA(totals_by_sector, sat_spec$DataYears[1], model)
+  }
+  # Check if disaggregation is needed based on model metadata
+  if(!is.null(model$specs$DisaggregationSpecs) & !is.null(sat$StaticFile)){
+    totals_by_sector <- disaggregateSatelliteTable(model, totals_by_sector, sat)
+  }
+  return(totals_by_sector)
 }
