b8970720fc100b36c00ac9dff1ddc8c6b424fabc
WesIngwersen <ingwersen.wesley@epa.gov>
2021-01-20 15:41:19 -0500

    Change model$demands to model$DemandVectors$vectors and model$DemandVectors$meta to be consistent with yml and model$Indicators

--- R/LoadDemandVectors.R
+++ R/LoadDemandVectors.R
@@ -1,22 +1,32 @@
-#' Adds demand vectors based on model specs to model object
+#' Adds demand vectors and metadata based on model specs to model object
 #' @param model A model list object with the specs object listed
-#' @return eeio model with a list of demand vectors added
-#' @format A list with lists of totals by sector and formatted satellite tables
+#' @return model with a list of demand vectors and a meta file
 #' @export
 loadDemandVectors <- function(model) {
   logging::loginfo("Loading demand vectors from model spec ...")
-  model$demands <- list()
-  for (v in names(model$specs$DemandVectors)) {
+  model$DemandVectors <- list()
+  meta <- data.frame()
+  model$DemandVectors$vectors <- list()
+  specs <- model$specs$DemandVectors
+  for (v in names(specs)) {
+
+    # Populate metadata
+    i <- specs[[v]]
+    i["Type"] <- v
+    meta <- rbind(meta,data.frame(i))
+
     #Check if the demand is registered
     if (v %in% names(dem_vec_fxn_registry)) {
        func_to_eval <- dem_vec_fxn_registry[[v]]
        demandFunction <- as.name(func_to_eval)
        dv <- do.call(eval(demandFunction), list(model))
-       model$demands[[v]] <- dv
+       model$DemandVectors$vectors[[v]] <- dv
        logging::loginfo(paste("Loaded",v,"demand vector."))
     } else {
       logging::logwarn(paste(v, "not found in registered demand vector functions. Not including", v))
     }
   }
+  model$DemandVectors$meta <- meta
   return(model)
 }
+  
