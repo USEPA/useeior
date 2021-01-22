7e463e95fa1da5e4a4882a716de5e1e7b8c8d142
WesIngwersen <ingwersen.wesley@epa.gov>
2021-01-22 13:59:21 -0500

    Update USEEIOv2.0 example

--- examples/USEEIO20.R
+++ examples/USEEIO20.R
@@ -1,10 +1,6 @@
 # Example for running useeior, see https://github.com/USEPA/useeior for more information on the model
 # Run ?functionname to see documentation and function input options for the functions in this example
-# The output csv files are the inputs for useeiopy (https://github.com/USEPA/useeiopy)
 
-# Start clean by removing useeior package and installing latest version
-remove.packages("useeior")
-devtools::install_github("USEPA/useeior")
 
 library(useeior)
 
@@ -21,5 +17,4 @@ result <- useeior::calculateEEIOModel(model, perspective='DIRECT',demand="Consum
 # Write model for API
 useeior::writeModelforAPI(model)
 
-# Write model matrices to csv for review
-useeior::writeModelMatrices(model,getwd())
+
