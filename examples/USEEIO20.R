f364b0111c3872c27261e52ab64e356040d3100a
MoLi7 <mo.vico.li@gmail.com>
2021-03-22 14:12:08 -0700

    Update USEEIO20.R example code

--- examples/USEEIO20.R
+++ examples/USEEIO20.R
@@ -4,15 +4,18 @@
 
 library(useeior)
 
-# See the versioning scheme for explanation of model names (https://github.com/USEPA/USEEIO/blob/master/VersioningScheme.md)
+# See all available model names
+# explanation of model names can be found at https://github.com/USEPA/USEEIO/blob/master/VersioningScheme.md
 useeior::seeAvailableModels()
-model <- useeior::loadIOData('USEEIOv2.0')
-model <- useeior::loadandbuildSatelliteTables(model)
-model <- useeior::loadandbuildIndicators(model)
-model <- useeior::loadDemandVectors(model)
-model <- useeior::buildEEIOModel(model)
 
-result <- useeior::calculateEEIOModel(model, perspective='DIRECT',demand="Consumption")
+# Build an EEIO model
+model <- buildModel("USEEIOv2.0")
+
+# Calculate direct perspective LCI and LCIA
+result <- useeior::calculateEEIOModel(model, perspective = "DIRECT", demand = "Consumption")
+
+# Adjust N matrix (total environmental impacts per dollar) to 2018 dollar and purchaser price
+adjusted_result <- adjustMultiplierPrice("N", currency_year = 2018, purchaser_price = TRUE, model)
 
 # Write model for API
 useeior::writeModelforAPI(model,"../useeio_api/")
