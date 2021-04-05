e1963e5f29d978031c70d701e062c6bd0e7a3455
MoLi7 <mo.vico.li@gmail.com>
2021-04-05 09:41:50 -0700

    Delete unused formatting for IOMB and USEEIOPY functions

--- R/WriteModel.R
+++ R/WriteModel.R
@@ -44,48 +44,6 @@ writeModelMatrices <- function(model, outputfolder) {
   logging::loginfo(paste0("Model matrices written to ", modelfolder, "."))
 }
 
-
-#' Write model components to output folder for useeiopy building using IO-Model-Builder format.
-#' @param model A complete EEIO model: a list with USEEIO model components and attributes.
-#' @description Only writes model economic components (DRC, Marketshares, Demand) for now.
-#' @export
-writeModelforUSEEIOPY <- function(model) {
-  user_dir <- rappdirs::user_data_dir()
-  modelfolder <- file.path(user_dir, "USEEIO", "Model_Builds", model$specs$Model)
-  if (!dir.exists(modelfolder)) {
-    dir.create(modelfolder, recursive = TRUE) 
-  }
-  # Sat Tables
-  sattable <- do.call(rbind.data.frame, model$SatelliteTables$coeffs_by_sector)
-  # LCIA
-  LCIA <- formatLCIAforIOMB(model)
-  # Sector meta data
-  SectorMetaData <- formatSectorMetaDataforIOMB(model)
-  # Demand
-  if(model$specs$PrimaryRegionAcronym=="US") {
-    Demand <- getUSTotalConsProd(model)
-    #add in food system demand
-    #Demand <- addDemandforSubsystem("food",Demand)
-  }
-  # Format DRC for IOMB
-  DirectRequirementsCoefficients <- formatIOTableforIOMB(model$A, model)
-  # Write model build components to csv
-  name_pre <- paste(modelfolder, model$specs$Model, sep = "/")
-  utils::write.csv(sattable, paste0(name_pre, "_sat.csv"), na = "", row.names = FALSE, fileEncoding = "UTF-8") 
-  utils::write.csv(LCIA, paste0(name_pre, "_LCIA.csv"), row.names = FALSE, fileEncoding = "UTF-8")
-  utils::write.csv(SectorMetaData, paste0(name_pre, "_sector_meta_data.csv"), row.names = FALSE, fileEncoding = "UTF-8")
-  utils::write.csv(f, paste0(name_pre, "_FinalDemand.csv"), row.names = FALSE, fileEncoding = "UTF-8")
-  utils::write.csv(DirectRequirementsCoefficients, paste0(name_pre, "_DRC.csv"), row.names = TRUE, fileEncoding = "UTF-8") #DRC needs row indices
-  # Write logs to file in Model Builds folder
-  logtimestamp <- Sys.Date()
-  #if (!dir.exists())
-  #dir.create("modelbuildlogs", recursive = TRUE) # meant to be flexible up to users
-  #logfilename <- paste0("modelbuildlogs/", model$specs$Model, logtimestamp, ".log")
-  #logging::addHandler(logging::writeToFile, file = logfilename, level = "INFO")
-  logging::loginfo(paste0("Model components written to ",modelfolder," ."))
-}
-
-
 ###All functions below here are internal
 
 #' Sets directories to write model output data to
