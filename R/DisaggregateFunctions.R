c7090ac707fab030798bb543eb60a6e20166c60a
jvendries <jvendries@hotmail.com>
2021-04-22 19:33:39 -0400

    Changed disaggRatios used for allocating margins from industry to commodity ratios.

--- R/DisaggregateFunctions.R
+++ R/DisaggregateFunctions.R
@@ -112,7 +112,7 @@ disaggregateMargins <- function(model, disagg)
   originalIndex <-  grep(disagg$OriginalSectorCode, model$Margins$Code_Loc)#get row index of the original aggregate sector in the model$Margins object
   originalRow <- model$Margins[originalIndex,]#copy row containing the Margins information for the original aggregate sector
   disaggMargins <-originalRow[rep(seq_len(nrow(originalRow)), length(disagg$DisaggregatedSectorCodes)),,drop=FALSE]#replicate the original a number of times equal to the number of disaggregate sectors
-  disaggRatios <- unname(disaggregatedRatios(model, disagg, "Industry"))#ratios needed to calculate the margins for the disaggregated sectors. Need to unname for compatibility with Rho matrix later in the model build process.
+  disaggRatios <- unname(disaggregatedRatios(model, disagg, "Commodity"))#ratios needed to calculate the margins for the disaggregated sectors. Need to unname for compatibility with Rho matrix later in the model build process.
   
   #variable to determine length of Code substring, i.e., code length minus geographic identifer and separator character (e.g. "/US")
   codeLength <- nchar(disagg$DisaggregatedSectorCodes[1])-nchar(model$specs$PrimaryRegionAcronym) - 1
