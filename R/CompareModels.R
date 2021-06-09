bdaedb4d3459df99204e478c137f157d34bdac1d
MoLi7 <mo.vico.li@gmail.com>
2021-06-09 11:55:52 -0700

    Update compareFlowTotals function

--- R/CompareModels.R
+++ R/CompareModels.R
@@ -1,28 +1,24 @@
-#CompareModels.R
+# Functions for comparing models
 
 #' Compare flow totals for two models
 #' @param modelA, a useeior model
 #' @param modelB a useeior model 
-#' @return df_confrontation, the validate::confrontation result of the flow total comparison
+#' @return comparison, a list with pass/fail comparison results
 #' @export
 compareFlowTotals <- function(modelA, modelB) {
-  #Get flow totals for each model
+  # Get flow totals for each model
   A <- groupandsumTbSbyFlowLoc(modelA$TbS)
   B <- groupandsumTbSbyFlowLoc(modelB$TbS)
-
   # Generate a comparison to see if flow totals from two models are within 1%
-  rule <- validate::validator(abs(A - B)/A <= 0.01)
-  confrontation <- validate::confront(A, rule, B)
-  summary(confrontation)
-  df_confrontation <- validate::as.data.frame(confrontation)
-  return(df_confrontation)
+  rel_diff <- (A - B)/B
+  comparison <- formatValidationResult(rel_diff, abs_diff = TRUE, tolerance = 0.01)
+  return(comparison)
 }
 
 #' Sum totals_by_sector by flow and location
 #' @param TbS a totals-by-sector list from a model
 #' @return flowtotal, a df with 1 column of flowamount with a total for that flow given in rowname
 groupandsumTbSbyFlowLoc <- function(TbS) {
-  #tbs <- data.frame(do.call("rbind", TbS))
   fields <- c("Flowable","Context","Unit","Location")
   TbS$FlowLoc <- apply(TbS[, fields],1,FUN = joinStringswithSlashes)
   TbS <- dplyr::group_by(TbS,FlowLoc) 
