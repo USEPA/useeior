54110612ca6b7e7b2fbee107489843ced4c0954a
Ben Young <Ben.Young@erg.com>
2021-09-29 12:55:14 -0400

    generate new crosswalk column with model schema when disaggregation present

--- R/InitializeModel.R
+++ R/InitializeModel.R
@@ -16,6 +16,12 @@ initializeModel <- function(modelname) {
                            as.environment("package:useeior"))
     model$crosswalk <- unique(model$crosswalk[, c("NAICS_2012_Code", colnames(model$crosswalk)[startsWith(colnames(model$crosswalk), "BEA")])])
     colnames(model$crosswalk) <- gsub(paste0("_", model$specs$BaseIOSchema, "|_Code"), "", colnames(model$crosswalk))
+    rownames(model$crosswalk) <- NULL
+    if(!is.null(model$specs$DisaggregationSpecs)){
+      modelschema <- generateModelSectorSchema(model)
+      baseschema <- paste0("BEA_", model$specs$BaseIOLevel)
+      model$crosswalk[modelschema] <- model$crosswalk[baseschema]
+    }
   }
   return(model)
 }
