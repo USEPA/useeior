d3550cbc484c9406a1d9df54fc4e96b4f89a01ce
Ben Young <Ben.Young@erg.com>
2021-10-05 22:34:46 -0400

    use and rename the exisiting model crosswalk when writing to excel

--- R/WriteModel.R
+++ R/WriteModel.R
@@ -310,30 +310,11 @@ writeSessionInfotoFile <- function(path) {
 #' @return a data.frame, sectorcrosswalk table
 prepareModelSectorCrosswalk <- function(model) {
   crosswalk <- model$crosswalk
-  
-  # Check for disaggregation
-  if(!is.null(model$specs$DisaggregationSpecs)){
-    #create name to use as column header for disaggregated schema
-    SectorSchema <- generateModelSectorSchema(model)
-    #deterime which rows and columns to modify
-    cwColIndex <- match(paste0("BEA_", model$specs$BaseIOLevel), colnames(crosswalk))#search for concatenation of "BEA" and model$specs$BaseIOlevel object in crosswalk column names
-    
-    #copy relevant column over as last column
-    crosswalk <- cbind(model$crosswalk, model$crosswalk[, paste0("BEA_", model$specs$BaseIOLevel)])
-    colnames(crosswalk)[length(crosswalk)] <- SectorSchema #rename column
-    
-    #replace disaggregated sector codes in original column with original sector code (e.g. 562HAZ with 562000)
-    for (disagg in model$DisaggregationSpecs$Disaggregation){
-      OriginalCodeLength <- regexpr(pattern ='/',disagg$OriginalSectorCode) - 1 #used to determine the length of the sector codes. E.g., detail would be 6, while summary would generally be 3 though variable, and sector would be variable
-      DisaggCodeLength <- regexpr(pattern ='/',disagg$DisaggregatedSectorCodes[[1]]) - 1 #used to determine length of disaggregated sector codes.
-      
-      disaggNAICSIndex <- crosswalk[,cwColIndex] %in% substr(disagg$DisaggregatedSectorCodes,1,DisaggCodeLength) #find all the indeces in crosswalk where there disaggregated codes
-      crosswalk[disaggNAICSIndex,cwColIndex] <- substr(disagg$OriginalSectorCode,1,OriginalCodeLength) #replace the disaggregated codes with the original sector code
-      
-    }
-    
-    
-  }
+
+  #create name to use as column header for disaggregated schema
+  SectorSchema <- generateModelSectorSchema(model)
+  #rename "USEEIO" (the last column) as the sector schema 
+  colnames(crosswalk)[length(crosswalk)] <- SectorSchema #rename column
   
   return(crosswalk)
   
