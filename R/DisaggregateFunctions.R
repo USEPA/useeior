a6870c0c4941f4d785c026fa1cb32bf832534df5
jvendries <jvendries@hotmail.com>
2021-10-06 13:11:10 -0400

    Fixing function descriptions and removing block comments containing obsolete code.

--- R/DisaggregateFunctions.R
+++ R/DisaggregateFunctions.R
@@ -54,14 +54,12 @@ disaggregateModel <- function (model){
 #' @param model Model file loaded with IO tables
 #' @return A model with the specified aggregation and disaggregation specs.
 getDisaggregationSpecs <- function (model){
-  #model$DisaggregationSpecs$Aggregation <- vector(mode='list')
+
   model$DisaggregationSpecs$Disaggregation <- vector(mode='list')
   for (configFile in model$specs$DisaggregationSpecs){
     logging::loginfo(paste0("Loading disaggregation specs for ", configFile, "..."))
     config <- getConfiguration(configFile, "disagg")
-    # if('Aggregation' %in% names(config)){
-    #   model$DisaggregationSpecs$Aggregation <- append(model$DisaggregationSpecs$Aggregation, config$Aggregation)
-    # }
+
     if('Disaggregation' %in% names(config)){
       model$DisaggregationSpecs$Disaggregation <- append(model$DisaggregationSpecs$Disaggregation, config$Disaggregation)
     }
