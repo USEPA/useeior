901e6a99180f3097526d23f7096cfc4ce302dda7
Ben Young <Ben.Young@erg.com>
2022-07-07 21:59:05 -0400

    adjust CbS to use industry output for WIO sectors

--- R/SatelliteFunctions.R
+++ R/SatelliteFunctions.R
@@ -71,11 +71,21 @@ generateFlowtoDollarCoefficient <- function (sattable, outputyear, referenceyear
   rownames(Output_adj) <- gsub(paste0("/", location_acronym), "", rownames(Output_adj))
   # Merge the satellite table with the adjusted industry output
   Sattable_USEEIO_wOutput <- merge(sattable, Output_adj, by.x = "Sector", by.y = 0, all.x = TRUE)
-  # Drop rows where output is zero
   outputcolname <- paste0(outputyear, output_type, "Output")
-  Sattable_USEEIO_wOutput <- Sattable_USEEIO_wOutput[Sattable_USEEIO_wOutput[, outputcolname] != 0, ]
+  if(model$specs$ModelType=="WIO"){
+    # Extract Industry output from the model for WIO sectors as these will not be found in the BEA data
+    wio <- Sattable_USEEIO_wOutput[(Sattable_USEEIO_wOutput$Sector %in% model$WasteTreatmentIndustries$Code), ]
+    industry_output <- as.data.frame(model$IndustryOutput)
+    rownames(industry_output) <- gsub(paste0("/", location_acronym), "", rownames(industry_output))
+    wio <- merge(wio, industry_output, by.x = 'Sector', by.y = 0)
+    wio[[outputcolname]] <- wio[['model$IndustryOutput']]
+    wio[['model$IndustryOutput']] <- NULL
+    Sattable_USEEIO_wOutput <- rbind(Sattable_USEEIO_wOutput, wio)
+  }
   # Drop rows where output is NA
   Sattable_USEEIO_wOutput <- Sattable_USEEIO_wOutput[!is.na(Sattable_USEEIO_wOutput[, outputcolname]), ]
+  # Drop rows where output is zero
+  Sattable_USEEIO_wOutput <- Sattable_USEEIO_wOutput[Sattable_USEEIO_wOutput[, outputcolname] != 0, ]
   # Calculate FlowAmount by dividing the original FlowAmount by the adjusted industry output
   Sattable_USEEIO_wOutput$FlowAmount <- Sattable_USEEIO_wOutput$FlowAmount/Sattable_USEEIO_wOutput[, outputcolname]
   Sattable_USEEIO_wOutput[, outputcolname] <- NULL
