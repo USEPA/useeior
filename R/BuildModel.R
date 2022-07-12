e993991a1c93763e9deb8fbb5ebd36cdee6bf133
Ben Young <Ben.Young@erg.com>
2022-07-12 13:59:41 -0400

    skip C, D, and N when no indicators in model

--- R/BuildModel.R
+++ R/BuildModel.R
@@ -80,13 +80,15 @@ constructEEIOMatrices <- function(model) {
   logging::loginfo("Building B matrix (direct emissions and resource use per dollar)...")
   model$B <- createBfromFlowDataandOutput(model)
   
-  # Generate C matrix
-  logging::loginfo("Building C matrix (characterization factors for model indicators)...")
-  model$C <- createCfromFactorsandBflows(model$Indicators$factors,rownames(model$B))
+  if(!is.null(model$Indicators)) {
+    # Generate C matrix
+    logging::loginfo("Building C matrix (characterization factors for model indicators)...")
+    model$C <- createCfromFactorsandBflows(model$Indicators$factors,rownames(model$B))
 
-  # Add direct impact matrix
-  logging::loginfo("Calculating D matrix (direct environmental impacts per dollar)...")
-  model$D <- model$C %*% model$B 
+    # Add direct impact matrix
+    logging::loginfo("Calculating D matrix (direct environmental impacts per dollar)...")
+    model$D <- model$C %*% model$B
+  }
   
   # Calculate total emissions/resource use per dollar (M)
   logging::loginfo("Calculating M matrix (total emissions and resource use per dollar)...")
@@ -97,12 +99,13 @@ constructEEIOMatrices <- function(model) {
   model$M_d <- model$B %*% model$L_d
   colnames(model$M_d) <- colnames(model$M)
   
-  # Calculate total impacts per dollar (N), impact category x sector
-  logging::loginfo("Calculating N matrix (total environmental impacts per dollar)...")
-  model$N <- model$C %*% model$M
-  # Calculate U_d, the domestic impacts per dollar
-  logging::loginfo("Calculating N_d matrix (total environmental impacts per dollar from domestic activity)...")
-  model$N_d <- model$C %*% model$M_d
+  if(!is.null(model$Indicators)) {
+    # Calculate total impacts per dollar (N), impact category x sector
+    logging::loginfo("Calculating N matrix (total environmental impacts per dollar)...")
+    model$N <- model$C %*% model$M
+    logging::loginfo("Calculating N_d matrix (total environmental impacts per dollar from domestic activity)...")
+    model$N_d <- model$C %*% model$M_d
+  }
   
   # Calculate year over model IO year price ratio
   logging::loginfo("Calculating Rho matrix (price year ratio)...")
