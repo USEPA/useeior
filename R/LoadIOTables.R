74b82af355ba329bec2d640163c6897e977a653d
jvendries <jvendries@hotmail.com>
2022-07-28 02:26:27 -0400

    added function calls in LoadIOTables.R for MUIO models

--- R/LoadIOTables.R
+++ R/LoadIOTables.R
@@ -70,8 +70,10 @@ loadIOData <- function(model, configpaths = NULL) {
   }
   
   # Check for mixed units
-  if(model$specs$ModelType == "MixedUnits"){
+  if(model$specs$ModelType == "MUIO"){
     temp <- 1
+    model <- getMUIOSectors(model, configpaths)
+    model <- convertSectorsToPhysical(model, configpaths)
     
   }
     
