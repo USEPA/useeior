c7fef1a30d037a6930f18b4c340845aa3986a747
jvendries <jvendries@hotmail>
2024-01-12 18:53:36 -0500

    fix variable name to match return statement

--- R/DemandFunctions.R
+++ R/DemandFunctions.R
@@ -91,7 +91,7 @@ prepareDomesticProductionDemand <- function(model, location) {
 #' @return A named vector with demand
 prepareImportProductionDemand <- function(model, location) {
   if (model$specs$IODataSource == "stateior") {
-    y_d_p <- prepare2RDemand(model, location, domestic = FALSE)
+    y_m_p <- prepare2RDemand(model, location, domestic = FALSE)
     # stop("Import production demand not yet implemented for 2R models.")
   } else {
     # Note that model$mu (i.e., ITA) is not included in import production demand because it is included in Domestic Production Demand
