48b5c1e1cc0665bb2c6f6d74c1480bcae42f152b
jvendries <jvendries@hotmail>
2024-01-30 16:19:34 -0500

    add placeholders for SPA functions

--- R/SPAFunctions.R
+++ R/SPAFunctions.R
@@ -23,3 +23,35 @@ testDataTrees <- function() {
   print(acme)
   return(acme)
 }
+
+
+
+#' Wrapper function to run SPA with useeior model object
+#' @description Run a SPA based on given paramters
+#' @param model A complete EEIO model: a list with USEEIO model components and attributes.
+#' @param y Final demand value for which SPA will be run
+#' @param cut_off Value to determine point at which to cut-off small sub-trees (percent of total effects)
+#' @param T_max Max number of tiers to search
+#' @param indicator Indicator to return indicator results for ($, CO2, etc.)
+#' @return A completed SPA in a data.tree object
+runSPA <- function(model, y, cut_off = 0.01, T_max, indicator = "Economic"){
+  
+  # SPA_result <- SPAEIO(F, A, y, F_total, T_max, percent, filename, sectornames, thresh_banner)
+  
+  return(SPA_result)
+  
+}
+
+
+
+#' @description Begin SPA based on given parameters
+#' @param model A complete EEIO model: a list with USEEIO model components and attributes.
+#' @param y Final demand value for which SPA will be run
+#' @param cut_off Value to determine point at which to cut-off small sub-trees (percent of total effects)
+#' @param T_max Max number of tiers to search
+#' @param indicator Indicator to return indicator results for ($, CO2, etc.)
+#' @return A completed SPA in a data.tree object
+SPAEIO <- function(model, y, cut_off = 0.01, T_max, indicator = "Economic"){ #NEED TO MODIFY PARAMETERS, RIGHT NOW IT'S A COPY OF runSPA
+ 
+  
+}
